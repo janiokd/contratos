7d7b71e8a7872983206611825e5ae51d81b3a5dd
Julia Disconzi <judisconzi90@gmail.com>
2025-06-06 18:22:10 +0100

    ajustes global.ccc e layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,13 @@ import './globals.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import type { ReactNode } from 'react';
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+});
+
 
 export const metadata = {
   title: 'Meu Projeto',
@@ -19,7 +26,8 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
-      <body className="flex flex-col min-h-screen">
+     <body className={`flex flex-col min-h-screen ${poppins.className}`}>
+
         <Header />
         <main className="flex-grow w-full px-4 sm:px-6 lg:px-8">{children}</main>
         <Footer />
